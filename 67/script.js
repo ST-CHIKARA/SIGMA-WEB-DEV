32ac09cdf2ea6300691937245a8b885514ed5fc5
Sourabh Tomar <sourabhtomar6709@gmail.com>
2026-07-12 23:33:56 +0530

    feat(67): more info about DOM

---
+++ 67/script.js
@@ -0,0 +1,13 @@
+console.log("hallo")
+
+document.body.firstElementChild
+document.body.firstElementChild.children
+document.body.firstElementChild.children[0]
+document.body.firstElementChild.children[1]
+document.body.firstElementChild.children[2]
+document.body.firstElementChild.children[3]
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.firstElementChild.children[3].previousSibling // will just show text as the text is the previous sibling as we are talking about the whole sibling not just the element sibling
+document.body.children[1]
+document.body.children[1].rows
